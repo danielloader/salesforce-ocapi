98085b772a0cc1bed1efa6318dfa56c56d8cabf4
Daniel Loader <hello@danielloader.uk>
2020-12-14 14:44:15 +0000

    Fix missing import

--- salesforce_ocapi/utils/__init__.py
+++ salesforce_ocapi/utils/__init__.py
@@ -5,3 +5,4 @@ import salesforce_ocapi.utils.exceptions as OCAPIExceptions
 from salesforce_ocapi.utils.fs import *
 from salesforce_ocapi.utils.paginator import *
 from salesforce_ocapi.utils.request import *
+from salesforce_ocapi.utils.webdav import *
